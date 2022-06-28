c1329b1d08c5fb72f4a533fbf6d6552fdcf151d9
toptalentdev <toptal715@gmail.com>
2022-06-28 08:45:00 -0700

    change about.svelte

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,2 +1,5 @@
-<h1>About</h1>
-<p>I like to code</p>
+<div class="m-auto max-w-md w-full overflow-hidden">
+  <h1 class="text-2xl block font-bold py-6 text-gray-400 tracking-widest text-center">
+    I am a Full Stack Developer
+  </h1>
+</div>
