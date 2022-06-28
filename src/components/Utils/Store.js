2b458ff6cce18221415adad99b9ca4f05e8abaf2
toptalentdev <toptal715@gmail.com>
2022-06-28 07:05:52 -0700

    add event

--- src/components/Utils/Store.js
+++ src/components/Utils/Store.js
@@ -1,3 +1,15 @@
 import { writable } from 'svelte/store'
+import { v4 } from 'uuid'
 
-export const count = writable(0)
+export const count = writable([
+  {
+    id: v4(),
+    text: 'Meet John',
+    done: false
+  },
+  {
+    id: v4(),
+    text: 'Call Smith',
+    done: false
+  }
+])
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,12 +1,19 @@
 <script>
-  import { count } from '../components/Utils/Store';
+  import { count } from '../components/Utils/Store'
+  import { v4 } from 'uuid'
 
-  let countValue;
+  let items = [];
+  let itemText;
 
-  const unsubscribe = count.subscribe(value => countValue = value);
+  count.subscribe(value => items = value);
 
   const addItem = () => {
-    count.update(n => n + 1)
+    if(!itemText) return
+    count.update(items => items = [...items, {
+      id: v4(),
+      text: itemText,
+      done: false
+    }])
   }
 </script>
 
@@ -15,10 +22,9 @@
     Tailwind CSS Todos
   </h1>
   <div class="flex items-center justify-between relative">
-    <input class="p-4 pr-20 border-t-2 border-green-500 rounded bg-gray-900 text-white w-full shadow-inner outline-none" placeholder="Add new item..."/>
+    <input bind:value="{itemText}" class="p-4 pr-20 border-t-2 border-green-500 rounded bg-gray-900 text-white w-full shadow-inner outline-none" placeholder="Add new item..."/>
     <button on:click={addItem} class="text-green-400 hover:text-green-300 bg-gray-900 font-semibold py-2 px-4 absolute right-0 mr-2 focus:outline-none">
       Add
     </button>
   </div>
-  <div>{countValue}</div>
 </div>
